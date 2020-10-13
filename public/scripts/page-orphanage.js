892ec3d9f8761b8629c8b44e16a7483703b600d8
Rian dos Passos <riandpf2002@gmail.com>
2020-10-13 18:45:54 -0300

    nlw

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,46 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
+
+// Create map
+const map = L.map("mapid", options).setView([-28.677989, -49.3789228], 16);
+
+// Create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// Create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+// Create and add marker
+L.marker([-28.677989, -49.3789228], { icon }).addTo(map);
+
+/* Image gallery */
+
+function selectImage(event) {
+  const button = event.currentTarget;
+
+  // Remover todas as classes .active
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  // Selecionar a imagem clicada
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
+
+  // Atualizar o Container com a imagem
+  imageContainer.src = image.src;
+
+  // Adicionar a classe .active
+  button.classList.add("active");
+}
